b6bff015a06e01eb97010626c695210b0d87b91b
MoreiraRamires <62817742+MoreiraRamires@users.noreply.github.com>
2020-07-10 16:10:33 -0300

    consumi a primeira api

--- Exercicios/axios/main.js
+++ Exercicios/axios/main.js
@@ -1,6 +1,6 @@
 var axios = require("axios");
 
-axios.get("https://github.com/users/techtuxbr").then(function (data) {
-  console.log(data);
+axios.get(`https://api.github.com/users/MoreiraRamires`).then(function (response) {
+  console.log(response.data);
+  console.log(response.status);
 });
-
